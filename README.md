b89ccdb4ec89d90d38f4b7ae21570ca40e74ec82
Sue <cjscoding1997@gmail.com>
2021-10-05 23:48:46 +0900

    load data from json & display data in html

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Online_Shopping
-Online_Shopping_Cloning
+## Online_Shopping_Cloning
+
+___
+### HTML / CSS 
+- 기본 틀 작업
+
+### JSON
+- 비즈니스 로직과 데이터 분리시키기!
+    - js 파일에는 비즈니스 로직만 작성하고 데이터는 분리시켜 json 파일로 저장
---
+++ src/main.js
@@ -0,0 +1,37 @@
+//items 불러오는 함수
+function loadItem() {
+    //JSON file에서 아이템들 fetch하기
+    return fetch('data/data.json')
+        //fetch한 data를 promise형태로 반환해야 data사용이 가능하다
+        //.json() : data를 promise 형태로 반환해주는 res 객체의 method
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+//loadItem함수를 통해 받아온 인자값들을(items) 
+//html 페이지에 보이도록 구현하는 함수
+function displayItem(items) {
+    //items를 넣어줄 부모 요소 불러오기
+    const container = document.querySelector('.items');
+    //map으로 배열 내에 모든 요소에 대해 htmlString형식으로 만들어주고
+    //join으로 모든 아이템 html 합쳐서 부모 요소에 넣기
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//items html에 넣을 String 형태로 바꿔주는 함수
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item_desc">${item.gender}, ${item.size} size</span>
+    </li>
+    `;
+}
+
+//main
+loadItem()//아이템을 불러와서
+    .then(items => {//resolve 되었을 경우
+        displayItem(items);//보여주고
+        //setEventListeners(items);//이벤트 적용시키기
+    })
+    .catch(console.log);//reject 되었을 경우
